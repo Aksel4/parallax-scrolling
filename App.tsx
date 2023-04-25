dee1694df30fe193a5c343c3f30e128c0e37be26
Aksel4 <122082774+Aksel4@users.noreply.github.com>
2023-04-25 13:53:28 +0200

    IT works!!

--- App.tsx
+++ App.tsx
@@ -5,29 +5,54 @@ import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 export default function App() {
   return (
     <div>
-      <Parallax pages={3}>
-        <ParallaxLayer speed={1} offset={0} className="top">
-          Hello world
-        </ParallaxLayer>
+      <div>elo</div>
+      <div>
+        <Parallax pages={5}>
+          <ParallaxLayer
+            speed={1}
+            offset={0}
+            className="top"
+            sticky={{ start: 0, end: 1 }}
+          >
+            Hello world
+          </ParallaxLayer>
 
-        <ParallaxLayer
-          speed={0.3}
-          className="bottom"
-          offset={1}
-          sticky={{ start: 1, end: 1.4 }}
-        >
-          Hello you
-        </ParallaxLayer>
+          <ParallaxLayer
+            speed={1.5}
+            className="bottom"
+            offset={1}
+            sticky={{ start: 1, end: 2 }}
+          >
+            Blender og 3D
+          </ParallaxLayer>
 
-        <ParallaxLayer
-          offset={2.4}
-          speed={0.3}
-          className="b2"
-          sticky={{ start: 2.4, end: 3.7 }}
-        >
-          Hola doroes et amet
-        </ParallaxLayer>
-      </Parallax>
+          <ParallaxLayer
+            offset={2.4}
+            speed={1.5}
+            className="b2"
+            sticky={{ start: 2, end: 3 }}
+          >
+            Konsept kunst
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={3}
+            speed={0.5}
+            className="b3"
+            sticky={{ start: 3, end: 4 }}
+          >
+            Poster art
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            speed={0.3}
+            sticky={{ start: 4, end: 4 }}
+            className="b4"
+          >
+            Kontakt meg
+          </ParallaxLayer>
+        </Parallax>
+      </div>
     </div>
   );
 }
